d2892f128cf8aadbd391d7b07046de34320d2496
Ronilo R <RoniloRagodos57@tamu.edu>
2015-11-05 00:11:43 -0600

    boundedbuf.h

---
+++ BoundedBuffer.h
@@ -0,0 +1,22 @@
+#ifndef BOUNDED_BUF_H
+#define BOUNDED_BUF_H
+
+#include "semaphore.h"
+#include <string>
+#include <vector>
+
+class BoundedBuffer {
+ public:
+  BoundedBuffer();
+  void add(std::string);
+  std::string remove();
+
+ private:
+  Semaphore empty;
+  Semaphore full;
+  Semaphore mutex;
+  
+  std::vector<std::string> data;
+}
+
+#endif
